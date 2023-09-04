64a5b352d32ea8bab7baa445cb81c52073a6e90a
Devshahmeer <theshahmeerfsd@gmail.com>
2023-09-04 21:44:30 +0500

    Accordion item w title, des & toggle functionality

---
+++ src/components/AccordionPops.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BsArrowDownCircle} from 'react-icons/bs';
+import {BsArrowUpCircle} from 'react-icons/bs';
+
+const AccordionPops = ({title, desc, active, setActive}) => {
+    return (
+        <div className='singleContainer'>
+            <span className={(active === title ? " activeTitle" : "") + " heading" + " flex"}>
+                {title}
+                <span className="headingIcon" onClick={() => setActive(title)}>
+                    {active === title ? (<BsArrowDownCircle className='icon' />) : 
+                    (<BsArrowUpCircle className='icon' />)}
+                </span>
+            </span>
+            <p className={(active === title ? "show" : '') + " description"}>
+                {desc}
+            </p>
+        </div>
+    )
+}
+
+export default AccordionPops
